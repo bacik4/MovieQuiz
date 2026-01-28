483ffaba9095a22f0f92033ad4a2e474ac1d679c
Igor <igorglebOv@yandex.ru>
2026-01-28 10:02:08 +0300

    delete comments

--- MovieQuizTests/MoviesLoaderTests.swift
+++ MovieQuizTests/MoviesLoaderTests.swift
@@ -9,7 +9,7 @@ import XCTest
 final class MoviesLoaderTests: XCTestCase {
     func testSuccessLoading() throws {
         // Given
-        let stubNetworkClient = StubNetworkClient(emulateError: false) // говорим, что не хотим эмулировать ошибку
+        let stubNetworkClient = StubNetworkClient(emulateError: false)
         let loader = MoviesLoader(networkClient: stubNetworkClient)
         
         // When
@@ -19,7 +19,6 @@ final class MoviesLoaderTests: XCTestCase {
             // Then
             switch result {
             case .success(let movies):
-                // давайте проверим, что пришло, например, два фильма — ведь в тестовых данных их всего два
                 XCTAssertEqual(movies.items.count, 2)
                 expectation.fulfill()
             case .failure(_):
@@ -32,7 +31,7 @@ final class MoviesLoaderTests: XCTestCase {
     
     func testFailureLoading() throws {
         // Given
-        let stubNetworkClient = StubNetworkClient(emulateError: true) // говорим, что хотим эмулировать ошибку
+        let stubNetworkClient = StubNetworkClient(emulateError: true)
         let loader = MoviesLoader(networkClient: stubNetworkClient)
         
         // When
